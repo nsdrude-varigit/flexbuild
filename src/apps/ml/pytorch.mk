c4ac8d1b78e1930c2c6624d5b61f44e9303644cd
Bruno Bonaldi Teixeira <bruno.t@variscite.com>
2025-06-09 13:40:03 -0300

    pytorch: align Makefile with pytorch-release changes
    The PyTorch demo requires version 2.7.1 of the torch package, but
    the current pytorch.mk file installs version 2.0.0 via a wheel package.
    This method is no longer supported as of commit nxp-imx/pytorch-release@012e780,
    which dropped support for wheel packages installation.
    
    PyTorch demo link:
    https://github.com/nxp-imx/pytorch-release/blob/012e780225f891b90747592e254b2399d6ec07fb/examples/pytorch_mobilenetv2.py
    
    This patch removes the torch package installation from
    pytorch.mk while retaining the installation of the example
    files.
    
    After applying this patch, torch must be installed manually
    using the following steps:
    
        python3 -m venv ~/venv
        source ~/venv/bin/activate
        pip3 install torch torchvision
    
    Signed-off-by: Bruno Bonaldi Teixeira <bruno.t@variscite.com>

--- src/apps/ml/pytorch.mk
+++ src/apps/ml/pytorch.mk
@@ -6,25 +6,13 @@
 
 # RDEPENDS: python3-core python3-numpy python3-future python3-typing-extensions numactl
 
-PV = "2.0.0"
-PYV = "cp311"
-PYTHON_SITEPACKAGES_DIR = "/usr/lib/python3.11/site-packages"
-
-
 pytorch:
 ifeq ($(strip $(subst ",,$(CONFIG_PYTORCH))),y)
 	@[ $(DESTARCH) != arm64 -o $(DISTROVARIANT) = tiny -o $(DISTROVARIANT) = base ] && exit || \
 	 $(call fbprint_b,"pytorch") && \
 	 $(call repo-mngr,fetch,pytorch,apps/ml) && \
 	 cd $(MLDIR)/pytorch && \
-	 install -d $(DESTDIR)/$(PYTHON_SITEPACKAGES_DIR) && \
 	 install -d $(DESTDIR)/usr/bin/pytorch/examples && \
-	 install -m 0555 examples/* $(DESTDIR)/usr/bin/pytorch/examples && \
-	 install -m 0555 src/build.sh $(DESTDIR)/usr/bin/pytorch && \
-	 pip3 install --upgrade --ignore-installed --disable-pip-version-check -v --platform linux_aarch64 \
-	      -t $(DESTDIR)/$(PYTHON_SITEPACKAGES_DIR) --no-cache-dir --no-deps whl/torch-$(PV)-$(PYV)*.whl && \
-	 mv $(DESTDIR)/$(PYTHON_SITEPACKAGES_DIR)/bin/* $(DESTDIR)/usr/bin && \
-	 rm -rf $(DESTDIR)/$(PYTHON_SITEPACKAGES_DIR)/bin && \
-	 rm -rf $(DESTDIR)/$(PYTHON_SITEPACKAGES_DIR)/torch/bin/test_cpp_rpc &&\
+	 install -m 0555 examples/pytorch_mobilenetv2.py $(DESTDIR)/usr/bin/pytorch/examples && \
 	 $(call fbprint_d,"pytorch")
 endif
