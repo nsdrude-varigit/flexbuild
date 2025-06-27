1aaa4e23252dddd224941f79cf35851e9bffdc1b
Luan Rafael Carneiro <luan.r@variscite.com>
2025-06-27 14:18:22 -0300

    bsp/sm: Add Makefile for System Manager
    The System Manager (SM) is an application that runs on a Cortex-M core found in
    many NXP i.MX processors. It is required to generate the flash.bin image for
    machines based on i.MX95.
    
    Signed-off-by: Luan Rafael Carneiro <luan.r@variscite.com>

---
+++ src/bsp/sm.mk
@@ -0,0 +1,25 @@
+#
+# Copyright 2025 Variscite
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+# build the System Manager for i.MX5 platforms
+
+SM_PLAT ?= mx95evk
+
+sm imx-sm:
+	@[ $${MACHINE:0:5} != imx95 ] && exit || \
+	if [ ! -d $(BSPDIR)/sm ]; then \
+		$(call fbprint_b,"imx-sm") && \
+		$(call repo-mngr,fetch,sm,bsp) \
+	fi && \
+        if [ ! -d $(PKGDIR)/apps/utils/cortexm_toolchain_cross/bin ]; then \
+                bld cortexm_toolchain_cross -r $(DISTROTYPE):$(DISTROVARIANT) -a $(DESTARCH); \
+        fi && \
+	cd $(BSPDIR)/sm && \
+	export SM_CROSS_COMPILE=$(PKGDIR)/apps/utils/cortexm_toolchain_cross/bin/arm-none-eabi- && \
+	make really-clean && \
+	make -j $(nproc --all) config=$(SM_PLAT) cfg && \
+	make -j $(nproc --all) config=$(SM_PLAT) img && \
+	$(call fbprint_d,"IMX SM for $(MACHINE)")
