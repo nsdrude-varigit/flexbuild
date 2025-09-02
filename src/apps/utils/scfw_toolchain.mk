74b7dc400c5b44e04bc714d2eb472b30d0a094af
Luan Rafael Carneiro <luan.r@variscite.com>
2025-09-02 12:40:07 -0300

    scfw_toolchain: Add documentation block
    Add a documentation block describing the scfw_toolchain target,
    including copyright and SPDX license identifier.
    
    Signed-off-by: Luan Rafael Carneiro <luan.r@variscite.com>

--- src/apps/utils/scfw_toolchain.mk
+++ src/apps/utils/scfw_toolchain.mk
@@ -1,3 +1,22 @@
+# Copyright 2025 Variscite Ltd.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# Target: scfw_toolchain
+#
+# Description:
+#   Download and install the ARM GCC 8-2018-q4-major toolchain
+#   (arm-none-eabi), required to build the System Controller
+#   Firmware (SCFW) for NXP i.MX8 platforms.
+#
+# Variables:
+#   SCFW_SUPPORT - If set to "false", this rule will exit without running.
+#
+# Notes:
+#   The SCFW requires this specific compiler version. Using a
+#   different arm-none-eabi release may lead to build errors or
+#   unexpected runtime issues.
+
 SCFW_SUPPORT ?= true
 
 scfw_toolchain:
